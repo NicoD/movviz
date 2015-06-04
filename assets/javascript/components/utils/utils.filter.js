4ce473df8ac89e2e542d86a75acd4eccee8040be
NicoD <n.denoual@gmail.com>
2015-06-05 00:45:59 +0200

    custom list pretty display

--- assets/javascript/components/utils/utils.filter.js
+++ assets/javascript/components/utils/utils.filter.js
@@ -3,7 +3,9 @@
 
   angular
     .module('movvizApp')
-    .filter('encode', encode);
+    .filter('encode', encode)
+    .filter('ucfirst', ucfirst)
+    .filter('ucword', ucword);
 
   function encode() {
     return function encodeURIFilter(input, rfc3986) {
@@ -12,6 +14,23 @@
         encoded = encoded.replace(/[!'()*]/g, escape);
       }
       return encoded;
+    };
+  }
+
+  function ucfirst() {
+    return function ucfirstFilter(input) {
+      return input.charAt(0).toUpperCase() + input.slice(1);
+    };
+  }
+
+  function ucword() {
+    return function ucwordFilter(input) {
+      var words = input.split(" ");
+      var nbWords = words.length;
+      for(var i=0; i<nbWords; i++) {
+        words[i] = ucfirst()(words[i]);
+      }
+      return words.join(" ");
     };
   }
 
