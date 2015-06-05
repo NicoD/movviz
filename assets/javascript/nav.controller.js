219bb19732d93dc2b4a3c3eab58bdcc1f7d42c2f
NicoD <n.denoual@gmail.com>
2015-06-05 18:55:24 +0200

    navbar with status + collapsed menu

---
+++ assets/javascript/nav.controller.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  angular
+  .module('movvizApp')
+  .controller('NavController', NavController);
+
+NavController.$inject = ['$scope', '$location'];
+function NavController($scope, $location) {
+  $scope.isActive = function(navpath) {
+    var path = $location.path();
+    if(path === '/') {
+      return navpath === path;
+    }
+    if((path == '/movies' || path.indexOf('/movies/') === 0 || path.indexOf('/movie/') === 0) &&
+       navpath == '/movies') {
+      return true;
+    }
+    return false;
+  };
+}
+
+
+}());
