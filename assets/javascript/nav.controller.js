128aa267ede937e694493a1beab17bf5719b4e6c
NicoD <n.denoual@gmail.com>
2015-06-05 19:47:04 +0200

    style improvments

--- assets/javascript/nav.controller.js
+++ assets/javascript/nav.controller.js
@@ -2,23 +2,24 @@
   'use strict';
 
   angular
-  .module('movvizApp')
-  .controller('NavController', NavController);
+    .module('movvizApp')
+    .controller('NavController', NavController);
 
-NavController.$inject = ['$scope', '$location'];
-function NavController($scope, $location) {
-  $scope.isActive = function(navpath) {
-    var path = $location.path();
-    if(path === '/') {
-      return navpath === path;
-    }
-    if((path == '/movies' || path.indexOf('/movies/') === 0 || path.indexOf('/movie/') === 0) &&
-       navpath == '/movies') {
-      return true;
-    }
-    return false;
-  };
-}
+  NavController.$inject = ['$scope', '$location'];
+
+  function NavController($scope, $location) {
+    $scope.isActive = function(navpath) {
+      var path = $location.path();
+      if(path === '/') {
+        return navpath === path;
+      }
+      if((path == '/movies' || path.indexOf('/movies/') === 0 || path.indexOf('/movie/') === 0) &&
+        navpath == '/movies') {
+        return true;
+      }
+      return false;
+    };
+  }
 
 
 }());
